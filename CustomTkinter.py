fb2ba8b0081dcbf372ae0d7b17a93e207396c482
PineAccessCreativeCollege <sydney.hanby-howard.226@accesscreative.ac.uk>
2025-02-03 20:41:45 +0000

    WHOOOHOOO Class system now in place

--- CustomTkinter.py
+++ CustomTkinter.py
@@ -1,64 +1,115 @@
 import customtkinter as ctk
 from tkinter import PhotoImage
 from PIL import Image, ImageTk
-import Profile
 
-def main():    
-    ######
-    #Root Window
-    ######
-       
-    root = ctk.CTk(screenName=None, baseName=None, useTk=1)
-    window_open=False
-    
-    def on_close():
-        print("Killing Window")
-        root.destroy()
+class Main(ctk.CTkFrame):
+    def __init__(self, master, width = 500, height = 800, corner_radius = None, border_width = None, bg_color = "transparent", fg_color = None, border_color = None, background_corner_colors = None, overwrite_preferred_drawing_method = None, **kwargs):
+        super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
         
-    def start_drag(event):
-        # Record the initial position when the mouse is pressed
-        global drag_data
-        drag_data = {'x': event.x, 'y': event.y}
-
-    def do_drag(event):
-        # Calculate the movement and move the window
-        delta_x = event.x - drag_data['x']
-        delta_y = event.y - drag_data['y']
-        new_x = root.winfo_x() + delta_x
-        new_y = root.winfo_y() + delta_y
-        root.geometry(f'+{new_x}+{new_y}')
-
-    
-    root.geometry("500x800")
-    root.overrideredirect(True)  # Remove title bar and "X" button
-
-    root.grid_columnconfigure(0, weight=1)
-    root.grid_columnconfigure(1, weight=0)
-    
-    def open_profile(root):
+        ######
+        #Root Window Settings
+        ######
+        
+        def on_close():
+            print("Killing Window")
+            self.master.destroy()
             
+        def open_profile_window():
+            print("Opening Profile Window")
+            profile_window = ProfileWindow(self)      
+            
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        
+        self.draggable = DraggableWindow(self.master)
+        
+        ##FRAMES
+
+        top_bar = ctk.CTkFrame(self, height=90, bg_color="transparent")#fg_color="transparent"
+        pokemon_profile = ctk.CTkFrame(self, height=250, width=300, bg_color="transparent")
+        search_results_f = ctk.CTkScrollableFrame(self, height=250, width=150, bg_color="transparent")
+        current_party = ctk.CTkFrame(self, height=250, bg_color="transparent")
+        
+        ##IMAGES
+        x_imagemage_path = "Red_X.png"
+        pill_image = Image.open(x_imagemage_path).resize((25,25))
+        XImage = ImageTk.PhotoImage(pill_image)
+        
+        ##BUTTONS
+        close_button = ctk.CTkButton(top_bar, text=None, width=70, image=XImage, bg_color="transparent",
+                                    fg_color="transparent", text_color="red", hover=None, command=on_close)
+        
+        profile_button = ctk.CTkButton(top_bar, width=50, height=50, text=None, 
+                                    fg_color="grey", hover=None, corner_radius=25, 
+                                    border_width=0, command=open_profile_window)
+        
+        
+        top_bar.bind("<ButtonPress-1>", self.draggable.start_drag)  # When mouse button is pressed
+        top_bar.bind("<B1-Motion>", self.draggable.do_drag)  # When mouse is moved with button pressed
+        
+        ##ENTRYS
+        search_box = ctk.CTkEntry(self, width=400, height=50, placeholder_text="Enter a Pokemon name or ID")
+        
+        ##LAYOUT
+        top_bar.grid(row=0, column=0, sticky="ew")
+        close_button.grid(row=0, column=0, sticky="w", padx=(0,370))
+        profile_button.grid(row=0, column=1, sticky="e", pady=10, padx=0)
+        search_box.grid(row=1, column=0, pady=20)
+        pokemon_profile.grid(row=2, column=0, sticky="w", padx=10)
+        search_results_f.grid(row=2, column=0, sticky="e", padx=10)
+        current_party.grid(row=3, column=0, sticky="ew", padx=10, pady=20)    
+        
+        
+        
+        
+        ##A simple algorithm for filling the buttons up inside the users party/ should
+        #make it easier to shift to a class based system for the buttons which should
+        #let them be changed on the fly and after login.
+        iterated_row = 0
+        iterated_column = 0
+        
+        for i in range(6):
+            i_poke_slot = 'pokemon_slot_' + str(i)
+            i_poke_slot = ctk.CTkButton(current_party, height=140, text=None)
+            if i == 3:
+                iterated_row = 1
+                iterated_column = 0
+            i_poke_slot.grid(row=iterated_row, column=iterated_column, padx=10, pady=10)
+            iterated_column+=1
+            
+            
+        ##An simpler algorithm for filling the search results frame with buttons
+        for i in range(10):
+            i_search_slot = 'search_result_' + str(i)
+            i_search_slot = ctk.CTkButton(search_results_f, text=None)
+            
+            i_search_slot.grid(row=i, column=0, padx=10, pady=10)
+
+class ProfileWindow(ctk.CTkToplevel):
+    def __init__(self, *args, fg_color = None, **kwargs):
+        super().__init__(*args, fg_color=fg_color, **kwargs)
+    
         ######
         #Profile Window
         ######
         
         print("HEy")
-        profile = ctk.CTkToplevel( root )
-        profile.transient( root )
-        
-        
+                
         ##SETUP WINDOW
-        profile.geometry("500x800")
-        profile.overrideredirect(True)  # Remove title bar and "X" button
+        self.geometry("400x700")
+        self.overrideredirect(True)  # Remove title bar and "X" button
 
-        profile.grid_columnconfigure(0, weight=1)
-        profile.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        
+        self.draggable = DraggableWindow(self)
         
         ##FUNCTIONS
         def on_close():
             global window_open
             window_open=False
             print(window_open)
-            profile.destroy()
+            self.destroy()
             
         def start_drag(event):
             # Record the initial position when the mouse is pressed
@@ -69,13 +120,13 @@ def main():
             # Calculate the movement and move the window
             delta_x = event.x - drag_data['x']
             delta_y = event.y - drag_data['y']
-            new_x = profile.winfo_x() + delta_x
-            new_y = profile.winfo_y() + delta_y
-            profile.geometry(f'+{new_x}+{new_y}')
+            new_x = self.winfo_x() + delta_x
+            new_y = self.winfo_y() + delta_y
+            self.geometry(f'+{new_x}+{new_y}')
 
         
         ##FRAMES
-        top_bar = ctk.CTkFrame(profile, height=120, bg_color="transparent")#fg_color="transparent"
+        top_bar = ctk.CTkFrame(self, height=120, bg_color="transparent")#fg_color="transparent"
 
         ##IMAGES
         ximagemage_path = "Red_X.png"
@@ -86,95 +137,36 @@ def main():
         close_button = ctk.CTkButton(top_bar, text=None, width=70, bg_color="transparent", image=XImage,
                                     fg_color="transparent", text_color="red", hover=None, command=on_close)
         
-        top_bar.bind("<ButtonPress-1>", start_drag)  # When mouse button is pressed
-        top_bar.bind("<B1-Motion>", do_drag)  # When mouse is moved with button pressed
+        top_bar.bind("<ButtonPress-1>", self.draggable.start_drag)  # When mouse button is pressed
+        top_bar.bind("<B1-Motion>", self.draggable.do_drag)  # When mouse is moved with button pressed
         
         ##LAYOUT
         top_bar.grid(row=0, column=0, sticky="ew")
-        close_button.grid(row=0, column=0, sticky="e")
+        close_button.grid(row=0, column=0, sticky="e", padx=(335,0), pady=10)
         
-        profile.mainloop()
-    
-    
-    ######
-    #More Profile Window
-    ######
-    
-    
-    def open_profile_root():
-        window_open=False
-        if window_open == False:
-            print(window_open)
-            window_open=True
-            Profile.open_profile(root)
-            
-        else:
-            print("Window not")
-            
+        self.mainloop()
 
-    ##FRAMES
-
-    top_bar = ctk.CTkFrame(root, height=90, bg_color="transparent")#fg_color="transparent"
-    pokemon_profile = ctk.CTkFrame(root, height=250, width=300, bg_color="transparent")
-    search_results_f = ctk.CTkScrollableFrame(root, height=250, width=150, bg_color="transparent")
-    current_party = ctk.CTkFrame(root, height=250, bg_color="transparent")
+class DraggableWindow():
+    def __init__(self, window):
+        self.window = window
+        self.drag_data = {'x': 0, 'y': 0}
+    def start_drag(self, event):
+        self.drag_data = {'x': event.x, 'y': event.y} 
     
-    ##IMAGES
-    ximagemage_path = "Red_X.png"
-    pill_image = Image.open(ximagemage_path).resize((25,25))
-    XImage = ImageTk.PhotoImage(pill_image)
+    def do_drag(self, event):
+        delta_x = event.x - self.drag_data['x']
+        delta_y = event.y - self.drag_data['y']
+        new_x = self.window.winfo_x() + delta_x
+        new_y = self.window.winfo_y() + delta_y
+        self.window.geometry(f'+{new_x}+{new_y}')
     
-    ##BUTTONS
-    close_button = ctk.CTkButton(top_bar, text=None, width=70, image=XImage, bg_color="transparent",
-                                 fg_color="transparent", text_color="red", hover=None, command=on_close)
-    
-    profile_button = ctk.CTkButton(top_bar, width=50, height=50, text=None, 
-                                   fg_color="grey", hover=None, corner_radius=25, 
-                                   border_width=0, command=open_profile_root)
-    
-    
-    top_bar.bind("<ButtonPress-1>", start_drag)  # When mouse button is pressed
-    top_bar.bind("<B1-Motion>", do_drag)  # When mouse is moved with button pressed
-     
-    ##ENTRYS
-    search_box = ctk.CTkEntry(root, width=400, height=50, placeholder_text="Enter a Pokemon name or ID")
-    
-    ##LAYOUT
-    top_bar.grid(row=0, column=0, sticky="ew")
-    close_button.grid(row=0, column=0, sticky="w")
-    profile_button.grid(row=0, column=1, sticky="e", padx=370, pady=10)
-    search_box.grid(row=1, columnspan=2, pady=20)
-    pokemon_profile.grid(row=2, column=0, sticky="w", padx=10)
-    search_results_f.grid(row=2, column=0, sticky="e", padx=10)
-    current_party.grid(row=3, column=0, sticky="ew", padx=10, pady=20)
-    
-    
-    ##A simple algorithm for filling the buttons up inside the users party/ should
-    #make it easier to shift to a class based system for the buttons which should
-    #let them be changed on the fly and after login.
-    iterated_row = 0
-    iterated_column = 0
-    
-    for i in range(6):
-        i_poke_slot = 'pokemon_slot_' + str(i)
-        i_poke_slot = ctk.CTkButton(current_party, height=140, text=None)
-        if i == 3:
-            iterated_row = 1
-            iterated_column = 0
-        i_poke_slot.grid(row=iterated_row, column=iterated_column, padx=10, pady=10)
-        iterated_column+=1
-        
-        
-    ##An simpler algorithm for filling the search results frame with buttons
-    for i in range(10):
-        i_search_slot = 'search_result_' + str(i)
-        i_search_slot = ctk.CTkButton(search_results_f, text=None)
-        
-        i_search_slot.grid(row=i, column=0, padx=10, pady=10)    
-    
-    ##Runs the Main Window
-        
+def main():
+    root=ctk.CTk()
+    root.overrideredirect(True)  # Remove title bar and "X" button
+    window = Main(root)
+    window.grid(row=0, column=0)
     root.mainloop()
     
 if __name__ == "__main__":
     main()
+    
